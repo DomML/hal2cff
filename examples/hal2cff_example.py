f514571b4d2f4604ee1f3b2ed9e3de57b7840c33
DML <dominique.mias@free.fr>
2021-11-19 15:17:27 +0100

    first commit

--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -1,8 +1,30 @@
+# +
 from hal2cff import hal2cff
+
+import ipywidgets as widgets
+from IPython.display import display, display_html
+# -
 
 # # hal2cff example
 
 
-print(hal2cff("https://hal.archives-ouvertes.fr/hal-01361430v1"))
+# +
+button = widgets.Button(description="Convert")
+url = widgets.Textarea(value = "https://hal.archives-ouvertes.fr/hal-01361430v1")
+Box = widgets.HBox([url,button])
+display(Box)
+
+output = widgets.Output()
+display(output)
 
 
+# -
+
+def run_and_display_query(_):
+    result = hal2cff(url.value)
+    with output:
+        output.clear_output()
+        display_html("<pre>" + result + "<\pre>", raw = True)
+
+
+button.on_click(run_and_display_query)
